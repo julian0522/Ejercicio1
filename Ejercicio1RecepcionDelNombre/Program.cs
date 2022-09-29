d46c0985e858325d24bee4da2676cdbe1e991f40
Julian Eduardo Guarnizo <julii2002@hotmail.com>
2022-09-29 13:32:49 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio1RecepcionDelNombre/Program.cs
@@ -0,0 +1,7 @@
+﻿// PROGRAMA QUE ALMACENA EL NOMMBRE DEL USUARIO ESCRITO EN LA CONSOLA
+// Y POSTERIOR A ELLO LO MUESTRA EN LA CONSOLA
+
+Console.WriteLine("Hola por favor digite  su nombre");
+
+String nombre = Console.ReadLine();
+Console.WriteLine("Hola, " + nombre);
